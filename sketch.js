bb6545091456eaf317c0197326783fd8acb870f4
vansh1425 <vanshsweety2005@gmail.com>
2020-11-26 16:54:10 +0530

    new bird added

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chuck;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,7 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    chuck=new Bird(50,200);
 }
 
 function draw(){
@@ -58,7 +59,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+     
+    chuck.display();
     bird.display();
     platform.display();
 }
